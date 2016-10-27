68693b14f61726eba077b978939718af8b280a0a
Young <dana.young@emc.com>
2016-10-26 23:04:12 -0700

    switch to new ECS 3.0 object store

--- munger1.js
+++ munger1.js
@@ -88,7 +88,7 @@ var AWS = require( "aws-sdk" ),
 // setup ECS config to point to Bellevue lab 
 var ECSconfig = {
   s3ForcePathStyle: true,
-  endpoint: new AWS.Endpoint('http://10.4.44.125:9020')
+  endpoint: new AWS.Endpoint('http://10.5.208.12:9020')
 };
 ECS.config.loadFromPath(__dirname + '/ECSconfig.json');
 var ecs = new ECS.S3(ECSconfig);
